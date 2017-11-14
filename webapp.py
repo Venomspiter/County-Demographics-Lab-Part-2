58ed63d72a80b53515f9f83382bc42b09b7746b9
McLeodN <31251637+mcleod805@users.noreply.github.com>
2017-11-14 09:25:58 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,9 +3,6 @@ import os
 import json
 app = Flask(__name__)
 
-with open('county_demographics.json') as demographics_data:
-       counties = json.load(demographics_data)
-
 @app.route("/")
 def render_main():
     return render_template('home.html', options = get_state_options())
@@ -16,6 +13,8 @@ def render_response():
     return render_template('home.html', options = get_state_options(),response = your_interesting_demographic_function2(state))
 def get_state_options():
     options = ""
+       with open('county_demographics.json') as demographics_data:
+              counties = json.load(demographics_data)
     state = []
     for c in counties
         if not c["State"] in state:
@@ -24,6 +23,6 @@ def get_state_options():
        options += Markup("<option value=\"" + s + "\">" + s + "</option>")
     return options
 def your_interesting_demographic_function(stateName):
-
+       return "hi"
 if __name__=="__main__":
     app.run(debug=False, port=54321)
