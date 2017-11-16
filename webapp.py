bf489f1743e873d852693df73947d2f5aad1343a
McLeodN <31251637+mcleod805@users.noreply.github.com>
2017-11-16 08:46:33 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,7 +1,25 @@
 from flask import Flask, request, Markup, render_template, Markup
 import os
 import json
-app = Flask(__name__)
+app = Flask(__name__"options = "")
+
+@app.route("/home")
+def render_response():
+    state = request.args["state"]
+    return render_template('home.html', options = get_state_options(),response = your_interesting_demographic_function2(state))
+def get_state_options():
+    options = ""
+       with open('county_demographics.json') as demographics_data:
+              counties = json.load(demographics_data)
+    state = []
+    for c in counties
+        if not c["State"] in state:
+              state.append(c["State"])
+    for s in state:
+       options += Markup("<option value=\"" + s + "\">" + s + "</option>")
+    return options
+def your_interesting_demographic_function(stateName):
+       return "hi"
 
 @app.route("/")
 def render_main():
