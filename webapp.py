9c206a655acb1a1bb58ee273081359de2cb8dfa6
McLeodN <31251637+mcleod805@users.noreply.github.com>
2017-11-25 11:29:29 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ def your_interesting_demographic_function(stateName):
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
     for c in counties:
-        if c["state"] in state:
+        if c["State"] in state:
                state.append(c["State"])
     return ["Income"]
 @app.route("/")
